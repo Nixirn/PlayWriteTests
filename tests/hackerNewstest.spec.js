518eb771e42a443448fcc4a0c33934ea13468095
Erick Lagunas <70537048+Nixirn@users.noreply.github.com>
2025-05-12 09:52:54 -0700

    Added logic to parse through the first 100 recent posts.

--- tests/hackerNewstest.spec.js
+++ tests/hackerNewstest.spec.js
@@ -7,6 +7,27 @@ test.beforeEach( async ({page}) => {
 
 test.describe('Ensure posts are ordered from most to least recent', () => {
     test('Verify order', async ({page}) => {
-        await page.getByRole('table').nth(2).locator("td").nth(1);
+        const timeAgoRegex = RegExp('\d{1,3}\s(minute ago)|(minutes ago)|(hour ago)|(hours ago)');
+        const postTable = await page.getByRole('table').nth(2).getByRole('row').filter({hasText: timeAgoRegex});
+        var all = await postTable.all()
+        for(let i = 0; i < 100; i++) {
+            let mod = i % 30;
+            let text = await (await all).at(mod)?.textContent()
+            if(mod == 0 && i > 0)  { // don't click more on first run
+                await page.getByRole('link', { name: 'More', exact: true }).click();
+                all = await postTable.all();
+                console.log(text); // TODO: Add helper methods to parse time and compare post time
+            } else {
+                console.log(text); // TODO: Add helper methods to parse time and compare post time
+            }
+        }
     })
 })
+
+const compareTime = (timeText, previous = "none") => {
+    
+}
+
+const parsePostTime = (postTimeText) => {
+
+}
