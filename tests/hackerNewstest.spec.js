4dfecf469ed0e81152a8f34372a03e864d699dd0
Erick Lagunas <70537048+Nixirn@users.noreply.github.com>
2025-05-13 10:06:50 -0700

    Added complete initial test.

--- tests/hackerNewstest.spec.js
+++ tests/hackerNewstest.spec.js
@@ -1,33 +1,60 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+const TIME_AGO_REGEX = RegExp(/\d{1,2}\s(minutes?|hours?)\s(ago)/);
+const MAX_TABLE_ENTRIES = 30;
+
 test.beforeEach( async ({page}) => {
     await page.goto('https://news.ycombinator.com/newest');
 })
 
 test.describe('Ensure posts are ordered from most to least recent', () => {
     test('Verify order', async ({page}) => {
-        const timeAgoRegex = RegExp('\d{1,3}\s(minute ago)|(minutes ago)|(hour ago)|(hours ago)');
-        const postTable = await page.getByRole('table').nth(2).getByRole('row').filter({hasText: timeAgoRegex});
-        var all = await postTable.all()
+        // Get the table that contains only the post entries with the time signatures on them
+        const postTable = await page.getByRole('table').nth(2).getByRole('row').filter({hasText: TIME_AGO_REGEX});
+        var all = await postTable.all();
+        var previousTime = -1;
+
+        // Loop through the first hundred entries 
         for(let i = 0; i < 100; i++) {
-            let mod = i % 30;
-            let text = await (await all).at(mod)?.textContent()
-            if(mod == 0 && i > 0)  { // don't click more on first run
+            let mod = i % MAX_TABLE_ENTRIES;
+
+            // Refresh Saved table when all entries have been read
+            if(mod == 0 && i > 0)  { 
                 await page.getByRole('link', { name: 'More', exact: true }).click();
                 all = await postTable.all();
-                console.log(text); // TODO: Add helper methods to parse time and compare post time
-            } else {
-                console.log(text); // TODO: Add helper methods to parse time and compare post time
             }
+
+            let text = await (await all).at(mod)?.textContent();
+            if(isOutOfOrder(parsePostTime(text), previousTime)) {
+                throw new Error("Found entry out of order!");
+            }
+            previousTime = parsePostTime(text);
         }
     })
 })
 
-const compareTime = (timeText, previous = "none") => {
-    
+const isOutOfOrder = (time, previous) => {
+    if(previous == -1) {
+        return false;
+    } else if (time - previous < 0) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 const parsePostTime = (postTimeText) => {
-
+    const matchArr = postTimeText.match(TIME_AGO_REGEX); // 0: full str, 1: time name 
+    switch (matchArr[1]) {
+        case 'minute':
+        case 'minutes':
+            return parseInt(matchArr[0]);
+        case 'hour':
+        case 'hours':
+            return parseInt(matchArr[0]) + 60;
+        default:
+            throw new Error("Parsed unrecognized time name: " + matchArr[1]);
+    }
 }
