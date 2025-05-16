45eb8255ac7267c0e79bb5ee14a3e0e93978ffc0
Erick Lagunas <70537048+Nixirn@users.noreply.github.com>
2025-05-16 10:54:13 -0700

    Added comments to code and changed active profiles

--- tests/hackerNewstest.spec.js
+++ tests/hackerNewstest.spec.js
@@ -8,47 +8,49 @@ test.beforeEach( async ({page}) => {
     await page.goto('https://news.ycombinator.com/newest');
 })
 
-test.describe('Ensure posts are ordered from most to least recent', () => {
-    test('Verify order', async ({page}) => {
-        // Get the table that contains only the post entries with the time signatures on them
-        const postTable = await page.getByRole('table').nth(2).getByRole('row').filter({hasText: TIME_AGO_REGEX});
-        var all = await postTable.all();
-        var previousTime = -1;
+test('Ensure posts are ordered from most to least recent', async ({page}) => {
+    // Get the table that contains only the post entries with the time signatures on them
+    const moreLink = await page.getByRole('link', { name: 'More'});
+    const postTable = await page.getByRole('table').nth(2).getByRole('row').filter({hasText: TIME_AGO_REGEX});
+    var all = await postTable.all();
+    var previousTime = -1;
 
-        // Loop through the first hundred entries 
-        for(let i = 0; i < 100; i++) {
-            let mod = i % MAX_TABLE_ENTRIES;
+    // Loop through the first hundred entries 
+    for(let i = 0; i < 100; i++) {
+        let mod = i % MAX_TABLE_ENTRIES;
 
-            // Refresh Saved table every time the maximum number of tables entries have been read
-            if(mod == 0 && i > 0)  { 
-                await page.getByRole('link', { name: 'More', exact: true }).click();
-                all = await postTable.all();
-            }
+        // Refresh Saved table every time the maximum number of tables entries have been read
+        if(mod == 0 && i > 0)  { 
+            await moreLink.click();
+            all = await postTable.all();
+        }
 
-            // Parse text and compare with previous time value. Save current time as previous for the next loop
-            let text = await (await all).at(mod)?.textContent();
+        // Parse text and compare with previous time value. Save current time as previous for the next loop
+        await all.at(mod)?.textContent().then((text) => {
             if(isOutOfOrder(parsePostTime(text), previousTime)) {
                 throw new Error("Found entry out of order!");
             }
             previousTime = parsePostTime(text);
-        }
-    })
+        });
+    }
+    await page.close();
 })
+
 
 const isOutOfOrder = (time, previous) => {
     // Return false if there is no previous (First entry)
     if(previous == -1) {
         return false;
     // Return true if the previous time value is greater then the current
-    } else if (time - previous < 0) {
-        return true;
+    } else if (time - previous < 0) { 
     } else {
         return false;
     }
 }
 
 const parsePostTime = (postTimeText) => {
-    const matchArr = postTimeText.match(TIME_AGO_REGEX); // 0: full str, 1: time name 
+    const matchArr = postTimeText.match(TIME_AGO_REGEX);
+     // 0: full str, 1: time name 
     // Increment time number based on time name
     switch (matchArr[1]) {
         case 'minute':
